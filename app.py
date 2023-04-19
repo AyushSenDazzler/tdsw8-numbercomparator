503b06a85435667553593e05819121fa758801b9
AYUSH SEN <85610426+AyushSenDazzler@users.noreply.github.com>
2023-04-19 21:56:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,25 +8,22 @@ This app shows largest among the given 3 numbers
 
 st.header('Input the numbers in the field below')
 
-def largest_number():
-    first_num = st.number_input("First Number")
-    second_num = st.number_input("Second Number")
-    third_num = st.number_input("Third Number")
+def largest_number(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+        
+first_num = st.number_input("First Number")
+second_num = st.number_input("Second Number")
+third_num = st.number_input("Third Number")
 
-    numbers = [first_num,second_num,third_num]
-
-    largest = numbers[0]
-
-    for num in numbers:
-        if num > largest:
-            largest = num
-            return largest
-
-st.write("""
-### The largest among all is : 
-""" largest)
-
-
+if st.button("Find largest number"):
+    result = largest_number(num1, num2, num3)
+    st.success("The largest among all is: {}".format(result))
+    
 
 
 
